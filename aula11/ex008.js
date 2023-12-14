8fb4099c64eafbf98de47ac2024af2374816e2fc
Witor Tenã <witortena@gmail.com>
2023-12-14 07:50:48 -0300

    Aula14 ex

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidde do seu carro  é ${vel} KM/h!`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
